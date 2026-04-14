453ae19b3a9ed2a02f660936a99e26c0854b6735
floortje tjeetes <florian.tjeertes@gmail.com>
2026-04-14 14:20:23 +0200

    update

---
+++ app/Models/Book.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Models;
+class Book{
+    public string $title;
+    public string $serie;
+    public double $volume
+
+}
+?>
--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -1,3 +1,17 @@
-<div>
-    <p>hi</p>
-</div>
+<section>
+      <?php if (isset($books)): ?>
+         <?php if($book->count < 0): ?>
+            no books to view
+         <?php else: ?>
+        
+             <?php foreach($books as $book){
+                
+                echo "$book->volume";
+                
+             }
+             ?>
+         <?php endif; ?>
+
+
+      <?php endif; ?>
+</section>
