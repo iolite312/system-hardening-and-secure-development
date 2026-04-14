9366cd53e9471792b34b99e8543071abe5df7c68
iolite312 <68647023+iolite312@users.noreply.github.com>
2026-04-14 15:29:02 +0200

    discard

--- resources/views/pages/home.php
+++ resources/views/pages/home.php
@@ -1,17 +1,4 @@
-<section>
-      <?php if (isset($books)): ?>
-         <?php if($book->count < 0): ?>
-            no books to view
-         <?php else: ?>
-        
-             <?php foreach($books as $book){
-                
-                echo "$book->volume";
-                
-             }
-             ?>
-         <?php endif; ?>
+<main>
+<h1>boeken</h1>
 
-
-      <?php endif; ?>
-</section>
+</main>
