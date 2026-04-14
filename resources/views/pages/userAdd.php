479acdc8462f46cbe6cf411af369c6e89601323e
PepijnKranenburg <641394@student.inholland.nl>
2026-04-14 14:21:27 +0200

    useradd

---
+++ resources/views/pages/userAdd.php
@@ -0,0 +1,20 @@
+<h1><?= $page ?></h1>
+
+<form method="POST" action="/add-user">
+    <div>
+        <label>Naam</label>
+        <input type="text" name="name" required>
+    </div>
+
+    <div>
+        <label>Email</label>
+        <input type="email" name="email" required>
+    </div>
+
+    <div>
+        <label>Wachtwoord</label>
+        <input type="password" name="password" required>
+    </div>
+
+    <button type="submit">Toevoegen</button>
+</form>
--- routes/public.php
+++ routes/public.php
@@ -14,3 +14,6 @@ $router->middleware(EnsureValidLogin::class, function () use ($router) {
     $router->get('/logout', [App\Controllers\LoginController::class, 'logout']);
     $router->get('/', [App\Controllers\HomeController::class, 'index']);
 });
+    $router->get('/', [App\Controllers\HomeController::class, 'index']);
+    
+    $router->get('/addUser', [App\Controllers\UserController::class, 'index']);
