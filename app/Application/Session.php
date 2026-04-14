f1c335d2146c355e328e0fb2cf80142faea59226
iolite312 <68647023+iolite312@users.noreply.github.com>
2026-04-14 13:37:24 +0200

    revert to normal session

--- app/Application/Session.php
+++ app/Application/Session.php
@@ -2,60 +2,45 @@
 
 namespace App\Application;
 
-use Ramsey\Uuid\Uuid;
-use App\Repositories\SessionHandlerRepository;
-
 class Session
 {
-    private static ?string $sessionId = null;
-    private static array $data = [];
-    private static SessionHandlerRepository $handler;
-
-    public static function start(?string $sessionId = null): void
+    public static function start(): void
     {
-        self::$handler = new SessionHandlerRepository();
-        self::$sessionId = $sessionId;
-        // Check if the session cookie exists
-        if (isset($_COOKIE['PHPSESSIDC']) || !is_null(self::$sessionId)) {
-            if (isset($_COOKIE['PHPSESSIDC'])) {
-                self::$sessionId = $_COOKIE['PHPSESSIDC'];
-            }
-            self::$data = self::$handler->read(self::$sessionId) ?: [];
-        } else {
-            // Generate a new session ID if no cookie is set
-            self::$sessionId = Uuid::uuid4()->toString();
-            self::$handler->writeNewSession(self::$sessionId);
-            setcookie('PHPSESSIDC', self::$sessionId, time() + 3600 * 24, '/');
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
         }
     }
 
     public static function destroy(): void
     {
-        if (self::$sessionId) {
-            // Clear session data
-            self::$handler->destroy(self::$sessionId);
-            setcookie('PHPSESSIDC', '', time() - 3600, '/');
-            self::$sessionId = null;
-            self::$data = [];
+        if (session_status() !== PHP_SESSION_ACTIVE) {
+            return;
         }
+
+        // Unset all session variables
+        $_SESSION = [];
+
+        // Destroy the session
+        session_destroy();
     }
 
     public static function get(string $key): mixed
     {
-        return self::$data[$key] ?? null;
+        return $_SESSION[$key] ?? null;
     }
 
     public static function getAll(): array
     {
-        return self::$data;
+        return $_SESSION ?? [];
     }
 
     public static function set(string $key, mixed $value): void
     {
-        self::$data[$key] = $value;
-        if (self::$sessionId) {
-            // Persist session data using the handler
-            self::$handler->write(self::$sessionId, self::$data);
-        }
+        $_SESSION[$key] = $value;
+    }
+
+    public static function remove(string $key): void
+    {
+        unset($_SESSION[$key]);
     }
 }
